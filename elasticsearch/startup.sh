63ecc7bb4b4232b41b2926ed62ed54006a602444
Neil Brown <hopoffbaby@googlemail.com>
2018-03-17 20:02:47 +0000

    Add docker files to start elastic search on rpi

---
+++ elasticsearch/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t neil/elasticsearch .
+docker run -d -p 9200:9200 -v /home/hopoffbaby/githome/rpielk/elasticsearch/config/:/usr/share/elasticsearch/conf/ -v /home/hopoffbaby/githome/rpielk/elasticsearch/data/:/usr/share/elasticsearch/data/ --name elastic neil/elasticsearch
