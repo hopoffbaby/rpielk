88ab180885d9143be56c6e12c2b4447b63191dd6
Neil Brown <hopoffbaby@googlemail.com>
2018-03-20 21:12:04 +0000

    added cpu restrictions to elasticsearch

--- elasticsearch/startup.sh
+++ elasticsearch/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker build -t neil/elasticsearch .
-docker run -d -p 9200:9200 -v /home/hopoffbaby/githome/rpielk/elasticsearch/config/:/usr/share/elasticsearch/conf/ -v /home/hopoffbaby/githome/rpielk/elasticsearch/data/:/usr/share/elasticsearch/data/ --name elastic neil/elasticsearch
+docker run -d --cpus="3.5" -p 9200:9200 -v /home/hopoffbaby/githome/rpielk/elasticsearch/config/:/usr/share/elasticsearch/conf/ -v /home/hopoffbaby/githome/rpielk/elasticsearch/data/:/usr/share/elasticsearch/data/ --name elastic neil/elasticsearch
+#docker run -d -p 9200:9200 -v /home/hopoffbaby/githome/rpielk/elasticsearch/data/:/usr/share/elasticsearch/data/ --name elastic neil/elasticsearch
